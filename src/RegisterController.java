1ec48012a709eff05b1d67a76777e49491949750
SpursFan21 <duncanfrederic@gmail.com>
2024-08-26 20:43:59 +1200

    Register created

--- src/RegisterController.java
+++ src/RegisterController.java
@@ -1,5 +1,18 @@
+// src/RegisterController.java
 package src;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegisterController {
-    
+    @FXML
+    private TextField usernamField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void handleRegister(){
+        System.out.println("Register clicked");
+    }
 }
